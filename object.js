93af7cf8119a8b4cf2ebd8d4867ae7be8b39e041
tilav <tilav@tilav.tilav>
2023-04-28 22:31:00 +0500

    js Object lesson

---
+++ object.js
@@ -0,0 +1,112 @@
+let movies = [
+    {
+      name: 'Inception',
+      year: 2010,
+      director: 'Christopher Nolan'
+    },
+    {
+      name: 'The Matrix',
+      year: 1999,
+      director: 'Lana Wachowski, Lilly Wachowski'
+    },
+    {
+      name: 'Interstellar',
+      year: 2014,
+      director: 'Christopher Nolan'
+    },
+    {
+      name: 'The Silence of the Lambs',
+      year: 1991,
+      director: 'Jonathan Demme'
+    },
+    {
+      name: 'The Shawshank Redemption',
+      year: 1994,
+      director: 'Frank Darabont'
+    },
+    {
+      name: 'The Godfather',
+      year: 1972,
+      director: 'Francis Ford Coppola'
+    },
+    {
+      name: 'The Dark Knight',
+      year: 2008,
+      director: 'Christopher Nolan'
+    },
+    {
+      name: 'The Godfather: Part II',
+      year: 1974,
+      director: 'Francis Ford Coppola'
+    },
+    {
+      name: '12 Angry Men',
+      year: 1957,
+      director: 'Sidney Lumet'
+    },
+    {
+      name: 'Forrest Gump',
+      year: 1994,
+      director: 'Robert Zemeckis'
+    },
+    {
+      name: 'Pulp Fiction',
+      year: 1994,
+      director: 'Quentin Tarantino'
+    },
+    {
+      name: 'The Lord of the Rings: The Fellowship of the Ring',
+      year: 2001,
+      director: 'Peter Jackson'
+    },
+    {
+      name: 'The Lord of the Rings: The Two Towers',
+      year: 2002,
+      director: 'Peter Jackson'
+    },
+    {
+      name: 'The Lord of the Rings: The Return of the King',
+      year: 2003,
+      director: 'Peter Jackson'
+    },
+    {
+      name: 'The Prestige',
+      year: 2006,
+      director: 'Christopher Nolan'
+    },
+    {
+      name: 'The Departed',
+      year: 2006,
+      director: 'Martin Scorsese'
+    },
+    {
+      name: 'Goodfellas',
+      year: 1990,
+      director: 'Martin Scorsese'
+    },
+    {
+      name: 'Fight Club',
+      year: 1999,
+      director: 'David Fincher'
+    },
+    {
+      name: 'The Social Network',
+      year: 2010,
+      director: 'David Fincher'
+    },
+    {
+      name: 'American Beauty',
+      year: 1999,
+      director: 'Sam Mendes'
+    },
+    {
+      name: 'The Green Mile',
+      year: 1999,
+      director: 'Frank Darabont'
+    },
+    {
+      name: 'Schindler\'s List',
+      year: 1993,
+      director: 'Steven Spielberg'
+    }
+]
